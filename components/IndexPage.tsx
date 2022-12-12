198d46239521cee2b7651e70baf24850fe57b0ef
Jacob Rose <jacobdanielrose@gmail.com>
2022-12-12 09:34:56 +0100

    removed banner

--- components/IndexPage.tsx
+++ components/IndexPage.tsx
@@ -34,7 +34,6 @@ export default function IndexPage(props: {
           )}
           {morePosts.length > 0 && <MoreStories posts={morePosts} />}
         </Container>
-        <IntroTemplate />
       </Layout>
     </>
   )
