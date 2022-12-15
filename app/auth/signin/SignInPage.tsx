3d85c59e859d36f12913f70f89b7011f453eeafb
Jacob Rose <jacobdanielrose@gmail.com>
2022-12-15 19:13:11 +0100

    temporarily disable

--- app/auth/signin/SignInPage.tsx
+++ app/auth/signin/SignInPage.tsx
@@ -1,27 +1,27 @@
-// import Container from 'components/BlogContainer'
-// import BlogHeader from 'components/BlogHeader'
-// import Layout from 'components/BlogLayout'
-// import { Settings, Social } from 'lib/sanity.queries'
-// import { getProviders } from "next-auth/react"
+import Container from 'components/BlogContainer'
+import BlogHeader from 'components/BlogHeader'
+import Layout from 'components/BlogLayout'
+import { Settings, Social } from 'lib/sanity.queries'
+import { getProviders } from "next-auth/react"
 
-// import SignInCard from './SignInCard'
+import SignInCard from './SignInCard'
 
-// export default function SignInPage(props: {
-//     providers: Awaited<ReturnType<typeof getProviders>>
-//     preview?: boolean
-//     loading?: boolean
-//     settings: Settings,
-//     socials: Social[],
-// }) {
-//     return (
-//         <Layout preview={null} loading={null}>
-//             <Container>
-//                 {/* @ts-ignore */}
-//                 <BlogHeader title={props.settings.title} level={2} socials={props.socials} />
-//                 <div className="flex justify-center mt-5">
-//                     <SignInCard providers={props.providers} callbackUrl="" />
-//                 </div>
-//             </Container>
-//         </Layout>
-//     )
-// }
+export default function SignInPage(props: {
+    providers: Awaited<ReturnType<typeof getProviders>>
+    preview?: boolean
+    loading?: boolean
+    settings: Settings,
+    socials: Social[],
+}) {
+    return (
+        <Layout preview={null} loading={null}>
+            <Container>
+                {/* @ts-ignore */}
+                <BlogHeader title={props.settings.title} level={2} socials={props.socials} />
+                <div className="flex justify-center mt-5">
+                    <SignInCard providers={props.providers} callbackUrl="" />
+                </div>
+            </Container>
+        </Layout>
+    )
+}
