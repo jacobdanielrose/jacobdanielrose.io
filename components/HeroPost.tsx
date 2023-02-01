0a6e90e4d127afa047f302ed132f6760dcfead40
Jacob Rose <jacobdanielrose@gmail.com>
2023-02-01 19:04:36 +0100

    changed links to point to blog

--- components/HeroPost.tsx
+++ components/HeroPost.tsx
@@ -19,7 +19,7 @@ export default function HeroPost(
       <div className="mb-20 md:mb-28 md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8">
         <div>
           <h3 className="mb-4 text-4xl leading-tight lg:text-6xl">
-            <Link href={`/posts/${slug}`} className="hover:border-b-2
+            <Link href={`/blog/${slug}`} className="hover:border-b-2
             hover:border-highlight">
               {title || 'Untitled'}
             </Link>
