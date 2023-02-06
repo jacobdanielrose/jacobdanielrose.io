bd932ed48438caf4995f63eb9e45a2583cf89903
Jacob Rose <jacobdanielrose@gmail.com>
2023-02-06 23:47:20 +0100

    quick fix

--- app/PreviewIndexPage.tsx
+++ app/PreviewIndexPage.tsx
@@ -1,7 +1,15 @@
 'use client'
 
+import { usePreview } from 'lib/sanity.preview'
+import {
+    type Post,
+    indexQuery,
+} from 'lib/sanity.queries'
+
 import IndexPage from './IndexPage'
 
 export default function PreviewIndexPage({ token }: { token: null | string }) {
-    return <IndexPage preview />
+    const posts: Post[] = usePreview(token, indexQuery) || []
+
+    return <IndexPage preview latestPost={posts[0]} />
 }
